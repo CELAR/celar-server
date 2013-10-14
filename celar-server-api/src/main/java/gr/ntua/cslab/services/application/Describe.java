68037a141b9608d3d3400bde64b4995d627592e8
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2013-10-14 16:26:06 +0300

    updated Describe xml

--- celar-server-api/src/main/java/gr/ntua/cslab/services/application/Describe.java
+++ celar-server-api/src/main/java/gr/ntua/cslab/services/application/Describe.java
@@ -2,9 +2,14 @@ package gr.ntua.cslab.services.application;
 
 import gr.ntua.cslab.descriptor.HTMLDescription;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +30,16 @@ public class Describe extends HttpServlet {
         desc = new HTMLDescription();
         desc.setName("Describe Application");
         desc.setRetValue("true/false");
-        desc.addParameter("", "");
+        desc.setDescription("This call is used to describe an application. Specifically with this call you can:"
+        		+ "<ul>"
+        		+ "<li>Load a TOSCA file using description parameter (see params for more)</li>"
+        		+ "</ul>");
+        desc.addParameter("description", "TOSCA File containing the Application Structure");
+        desc.setType("POST");
+        desc.setExample("http://83.212.124.172:8080/celar-server-api/application/describe/<br/>"
+        		+ "description=foo --POST argument--<br/>"
+        		+ "foo here the file content to be uploaded.<br/>"
+        		+ "<span style='color:red; font-weight:bold;'>WARNING: please URLEncode the file content BEFORE sending it</span>");
     }
 
 	/**
@@ -43,7 +57,20 @@ public class Describe extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+//		BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream()));
+//		ServletOutputStream out = response.getOutputStream();
+//		while(in.ready()){
+//			out.print(in.readLine());
+//		}
+//		out.println(request.getQueryString());
+		if(request.getParameter("description")!=null){
+			PrintStream file = new PrintStream(new FileOutputStream("/tmp/tosca.xml"), true);
+			file.println(request.getParameter("description"));
+			file.close();
+			response.getOutputStream().println("{\"status\" : \"saved\"}");
+		} else {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+		}
 	}
 
 }
