a94592096459874db3ed5f62f95d6d15578c47ea
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2014-06-17 17:13:44 +0300

    removed header

--- celar-server-daemon/src/main/java/gr/ntua/cslab/celar/server/daemon/rest/beans/ServerInfo.java
+++ celar-server-daemon/src/main/java/gr/ntua/cslab/celar/server/daemon/rest/beans/ServerInfo.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package gr.ntua.cslab.celar.server.daemon.rest.beans;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -11,8 +5,9 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
- * @author giannis
+ * Server info bean, holding information about the server. Not crucial, will be
+ * implemented later on.
+ * @author Giannis Giannakopoulos
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
