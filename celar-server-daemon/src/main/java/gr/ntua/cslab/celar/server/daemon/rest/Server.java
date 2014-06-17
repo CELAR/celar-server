86fb34c0b5fb5dd1873e2cfe71b29d5f494fb4ea
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2014-06-17 17:22:58 +0300

    removed explicit declaration of consume/produce media types

--- celar-server-daemon/src/main/java/gr/ntua/cslab/celar/server/daemon/rest/Server.java
+++ celar-server-daemon/src/main/java/gr/ntua/cslab/celar/server/daemon/rest/Server.java
@@ -4,8 +4,6 @@ import gr.ntua.cslab.celar.server.daemon.rest.beans.ServerInfo;
 import java.io.IOException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -17,7 +15,7 @@ public class Server {
     
     @GET
     @Path("info/")
-    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+//    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
     public ServerInfo getServerInfo() throws IOException {
         ServerInfo serverInfo;
         serverInfo = new ServerInfo();
