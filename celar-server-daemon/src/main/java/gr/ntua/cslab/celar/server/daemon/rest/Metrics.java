115f4e5f36de3c1d497538c80f416ead69b23eda
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2015-07-22 14:08:23 +0300

    added default value to token

--- celar-server-daemon/src/main/java/gr/ntua/cslab/celar/server/daemon/rest/Metrics.java
+++ celar-server-daemon/src/main/java/gr/ntua/cslab/celar/server/daemon/rest/Metrics.java
@@ -37,15 +37,15 @@ import static org.apache.log4j.Logger.getLogger;
  */
 @Path("/metrics/")
 public class Metrics {
-    
+
     static Logger LOG = getLogger(Metrics.class);
-    
+
     @POST
     @Path("put/")
     @Consumes(MediaType.APPLICATION_OCTET_STREAM)
-    public static Metric put(@Context HttpServletRequest request, InputStream input, @QueryParam("token") String token) throws JAXBException, DBException {
-        if(!token.equals("4c021c79-6862-4337-bc24-923320e41354")) {
-            ResponseBuilderImpl responseBuilder =  new ResponseBuilderImpl();
+    public static Metric put(@Context HttpServletRequest request, InputStream input, @DefaultValue("xxx") @QueryParam("token") String token) throws JAXBException, DBException {
+        if (!token.equals("4c021c79-6862-4337-bc24-923320e41354")) {
+            ResponseBuilderImpl responseBuilder = new ResponseBuilderImpl();
             responseBuilder.status(javax.ws.rs.core.Response.Status.FORBIDDEN);
             responseBuilder.entity("Token not recognized");
             throw new WebApplicationException(responseBuilder.build());
@@ -57,58 +57,83 @@ public class Metrics {
         store(m);
         return m;
     }
-    
+
     @GET
     @Path("remove/{id}")
-    public static String remove(@PathParam("id") int id) throws JAXBException, DBException, Exception {
+    public static String remove(@PathParam("id") int id, @DefaultValue("xxx") @QueryParam("token") String token) throws JAXBException, DBException, Exception {
+        if (!token.equals("4c021c79-6862-4337-bc24-923320e41354")) {
+            ResponseBuilderImpl responseBuilder = new ResponseBuilderImpl();
+            responseBuilder.status(javax.ws.rs.core.Response.Status.FORBIDDEN);
+            responseBuilder.entity("Token not recognized");
+            throw new WebApplicationException(responseBuilder.build());
+
+        }
         Metric m = new Metric(id);
-        if(m.id==0) return "Metric("+id+") does not exist";
+        if (m.id == 0) {
+            return "Metric(" + id + ") does not exist";
+        }
         delete(m);
         return "OK";
     }
-    
+
     @GET
     @Path("{id}/values")
     public static REList<MetricValue> getValues(
             @PathParam("id") int id,
             @DefaultValue("-1") @QueryParam("deployment_Id") String deploymentID,
             @DefaultValue("-1") @QueryParam("start_time") long start,
-            @DefaultValue("-1") @QueryParam("end_time") long end
-                              ) throws JAXBException, DBException, Exception {
+            @DefaultValue("-1") @QueryParam("end_time") long end,
+            @DefaultValue("xxx") @QueryParam("token") String token
+    ) throws JAXBException, DBException, Exception {
+        if (!token.equals("4c021c79-6862-4337-bc24-923320e41354")) {
+            ResponseBuilderImpl responseBuilder = new ResponseBuilderImpl();
+            responseBuilder.status(javax.ws.rs.core.Response.Status.FORBIDDEN);
+            responseBuilder.entity("Token not recognized");
+            throw new WebApplicationException(responseBuilder.build());
+
+        }
+
         Metric m = new Metric(id);
-        if(m.id==0) return null;
-        Timestamp tstart=null, tend =null;
-        if (start>=0) tstart = new Timestamp(0);
-        if (end>0) tend = new Timestamp(end);
+        if (m.id == 0) {
+            return null;
+        }
+        Timestamp tstart = null, tend = null;
+        if (start >= 0) {
+            tstart = new Timestamp(0);
+        }
+        if (end > 0) {
+            tend = new Timestamp(end);
+        }
         Deployment dep = null;
-        if(!deploymentID.equals("-1")) dep=getDeploymentById(deploymentID);
-        List<MetricValue> mvl = EntitySearchers.searchMetricValues(dep, m ,tstart, tend);
+        if (!deploymentID.equals("-1")) {
+            dep = getDeploymentById(deploymentID);
+        }
+        List<MetricValue> mvl = EntitySearchers.searchMetricValues(dep, m, tstart, tend);
         REList<MetricValue> rv = new REList();
         rv.values.addAll(mvl);
         return rv;
 
     }
-        
+
     @GET
     @Path("component/{component_id}")
-    public static REList<Metric> getComponentMetrics(  @PathParam("component_id") int id) throws Exception{
+    public static REList<Metric> getComponentMetrics(@PathParam("component_id") int id) throws Exception {
         Component c = new Component(id);
         return new REList(getMetrics(c));
     }
-    
+
     @POST
     @Path("values/put")
     @Consumes(MediaType.APPLICATION_OCTET_STREAM)
-    public static REList<MetricValue>  putMetrics(@Context HttpServletRequest request, InputStream input) throws JAXBException, DBException {
+    public static REList<MetricValue> putMetrics(@Context HttpServletRequest request, InputStream input) throws JAXBException, DBException {
         REList<MetricValue> metrics = new REList<>();
         metrics.unmarshal(input);
-        LOG.info("Received "+metrics.size()+ " metrics.");
-        for(Object o: metrics){
+        LOG.info("Received " + metrics.size() + " metrics.");
+        for (Object o : metrics) {
             MetricValue m = (MetricValue) o;
             store(m);
         }
         return metrics;
     }
 
-    
 }
