303c624c06ba1d2a0e4b78ce30ca9582a16762f9
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-08-19 15:01:35 +0300

    DeploymentInfo for deployment+state

--- celar-beans/src/main/java/gr/ntua/cslab/celar/server/beans/structured/DeploymentInfo.java
+++ celar-beans/src/main/java/gr/ntua/cslab/celar/server/beans/structured/DeploymentInfo.java
@@ -4,11 +4,16 @@ import gr.ntua.cslab.celar.server.beans.Deployment;
 import gr.ntua.cslab.celar.server.beans.DeploymentState;
 import gr.ntua.cslab.celar.server.beans.MyTimestamp;
 import java.util.Map;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * A Deployment with a Deployment State map and Timestamp
  * @author cmantas
  */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class DeploymentInfo extends Deployment{
     public Map stateMap;
     public MyTimestamp timestamp=null;
